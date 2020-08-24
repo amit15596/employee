85dd9da7f15b47f00aaa13514f57daf3b570816e
Kirtirajsinh Solanki <amit.mscit16@gmail.com>
2020-08-24 17:09:43 +0530

    add new routers

---
+++ app/routes/login.routes.js
@@ -0,0 +1,14 @@
+import express from 'express'
+import passport from 'passport'
+
+import loginController from '../controllers/login.controller';
+
+const loginRouter = express.Router()
+
+loginRouter.get('/api/v1/login', (req,res)=>{
+    req.render('/api/v1');
+})
+
+loginRouter.post('/api/v1/login', loginController.login)
+
+export default loginRouter
---
+++ app/routes/logout.routes.js
@@ -0,0 +1,10 @@
+import express from 'express'
+import passport from 'passport'
+import logoutController from '../controllers/logout.controller';
+
+
+const logoutRouter = express.Router()
+
+logoutRouter.get('/api/v1/logout', logoutController.logout())
+
+export default logoutRouter
