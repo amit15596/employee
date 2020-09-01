2861561dac9ee3ac1ac116a6f0ef5d0a7f78fc51
Kirtirajsinh Solanki <amit.mscit16@gmail.com>
2020-09-01 16:52:02 +0530

    add joi validation

--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -2,19 +2,19 @@
 import express from 'express'
 import passport from 'passport'
 
-
 // import local files
 import userController from '../controllers/user.controller'
+import userValidation from '../validation/user.validation'
 
 const userRouter = express.Router()
 
-userRouter.get('/api/v1/user', passport.authenticate('jwt', { session: false }), userController.getUserList);
+userRouter.get('/api/v1/user', passport.authenticate('jwt', { session: false }),userController.getUserList);
 
 userRouter.get('/api/v1/user/:id', passport.authenticate('jwt', { session: false }), userController.getUserDetails);
 
-userRouter.post('/api/v1/user',passport.authenticate('jwt', { session: false }) ,userController.addUserDetails);
+userRouter.post('/api/v1/user', passport.authenticate('jwt', { session: false }) ,userValidation, userController.addUserDetails);
 
-userRouter.put('/api/v1/user/:id',passport.authenticate('jwt', { session: false }) ,userController.updateUserDetails);
+userRouter.put('/api/v1/user/:id',passport.authenticate('jwt', { session: false }), userValidation ,userController.updateUserDetails);
 
 userRouter.delete('/api/v1/user/:id', passport.authenticate('jwt', { session: false }),userController.deleteUserDetails);
 
---
+++ app/validation/user.validation.js
@@ -0,0 +1,13 @@
+//import user from './validation/user.schema'
+const { user } = require('./user.schema')
+
+async function addUserValidation(req, res, next) {
+    const result = await user.validate(req.body);
+    if(result.error){
+        res.json({"status": 400,"error":"Error","Message": result.error.details[0].message});
+    } else{
+        next();
+    }
+}
+
+module.exports = addUserValidation;
