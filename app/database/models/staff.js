2b3a022a7b3034265dde293dc0d28472403f4eba
amit15596 <amit.mscit16@gmail.com>
2020-12-19 11:45:23 +0530

    add model

--- app/database/models/staff.js
+++ app/database/models/staff.js
@@ -1,13 +1,15 @@
 'use strict';
 
 module.exports = (sequelize,DataTypes)=> {
-  const staffs = sequelize.define('staffs',
+  const staff = sequelize.define('staffs',
   {
-      frist_name: DataTypes.STRING,
-      last_name: DataTypes.STRING,
+    name: DataTypes.STRING,
+    salary: DataTypes.STRING,
       email: DataTypes.STRING,
       password: DataTypes.STRING,
+      phone: DataTypes.STRING,
+      phone: DataTypes.STRING,
       phone: DataTypes.STRING
   },{});
-  return staffs;
+  return staff;
 };
